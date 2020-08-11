e0f145407346d704f1513164881971849ce3fc36
Zac Holland <zacharyholland@gmail.com>
2020-08-10 20:56:21 -0700

    POC working!

--- simple-transcoder/transcode-server/main.go
+++ simple-transcoder/transcode-server/main.go
@@ -5,27 +5,73 @@ import (
 	"log"
 	"net/http"
 	"os/exec"
-	// "time"
+	"encoding/json"
+	"strconv"
+	"math"
+	"time"
 	// "os"
 
 	// "github.com/anacrolix/torrent"
 )
 
-func fileHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("Starting transcode...")
+type Format struct {
+	Duration int `json:"duration"`
+}
+type Metadata struct {
+    Format Format `json:"format"`
+}
 
-	ffmpegArgs := []string{"-i", "http://localhost:3000", "-f", "matroska", "-c:v", "libx264", "-b", "300k", "-preset", "fast", "-tune", "zerolatency"}
-	time, ok := r.URL.Query()["time"]
+func fmtDuration(d time.Duration) string {
+	d = d.Round(time.Minute)
+	h := d / time.Hour
+	d -= h * time.Hour
+	m := d / time.Minute
+	d -= h * time.Minute
+	s := m / time.Second
+	return fmt.Sprintf("%02d:%02d:%02d", h, m, s)
+}
+
+func fileHandler(w http.ResponseWriter, r *http.Request) {
+	_, ok := r.URL.Query()["metadata"]
     if ok {
-		println(time)
-		ffmpegArgs = append(ffmpegArgs, "-ss", time[0])
+		// TODO: get real metadata
+		metadata := Metadata {
+			Format: Format{
+				Duration: 633,
+			},
+		}
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+		w.WriteHeader(http.StatusOK)
+		if err := json.NewEncoder(w).Encode(metadata); err != nil {
+			panic(err)
+		}
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	ffmpegArgs := []string{"-i", "http://localhost:3000", "-f", "matroska", "-c:v", "libx264", "-b", "300k", "-preset", "fast", "-tune", "zerolatency"}
+
+	timeArgs, ok := r.URL.Query()["time"]
+    if ok {
+		var timeFloat float64
+		timeFloat, err := strconv.ParseFloat(timeArgs[0], 64)
+		if err != nil {
+			timeFloat = 0
+		}
+		var timeInt int = int(math.Round(timeFloat))
+		timeDuration := time.Second * time.Duration(timeInt)
+		timeString := fmtDuration(timeDuration) 
+		ffmpegArgs = append(ffmpegArgs, "-ss", timeString)
 	}
 	ffmpegArgs = append(ffmpegArgs, "-")
 	
-	fmt.Println(ffmpegArgs)
-
 	cmdFF := exec.Command("ffmpeg", ffmpegArgs...)
-	defer cmdFF.Process.Kill()
+	defer func() {
+		// TODO: Why doesn't it ever close?
+		println("Closing ffmpeg...")
+		cmdFF.Process.Kill()
+	}()
 
 	cmdFF.Stdout = w
     if err := cmdFF.Run(); err != nil {
