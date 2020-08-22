0efd5ffeacf50b1ffd2ff048f8671acdbb8d50b9
Zac Holland <zachary@pubnub.com>
2020-08-22 12:28:35 -0700

    nicer layout movies

--- frontend/src/components/Navbar/index.js
+++ frontend/src/components/Navbar/index.js
@@ -5,11 +5,11 @@ import Nav from 'react-bootstrap/Nav';
 export default function App() {
     return (
         <Navbar bg="light" expand="lg">
-        <Navbar.Brand href="#home">Iceetime</Navbar.Brand>
+        <Navbar.Brand href="/">Iceetime</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="mr-auto">
-            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="/movies">Movies</Nav.Link>
             </Nav>
         </Navbar.Collapse>
         </Navbar>
--- frontend/src/pages/movies.js
+++ frontend/src/pages/movies.js
@@ -1,5 +1,17 @@
 import React from 'react';
 import { Redirect, Link } from 'react-router-dom';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+
+const styles = {
+  movieCard: {
+    paddingTop: "1em",
+    paddingBottom: "1em",
+  }
+};
 
 export default class MyComponent extends React.Component {
     constructor(props) {
@@ -10,7 +22,7 @@ export default class MyComponent extends React.Component {
         resp: null,
       };
     }
-  
+
     componentDidMount() {
       let tmdbAPIKey = process.env.REACT_APP_TMDB_API_KEY;
       fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${tmdbAPIKey}`)
@@ -30,7 +42,7 @@ export default class MyComponent extends React.Component {
           }
         )
     }
-  
+
     render() {
       const { error, isLoaded, resp, selectedMovie } = this.state;
       if (error) {
@@ -39,20 +51,47 @@ export default class MyComponent extends React.Component {
         return <div>Loading...</div>;
       } else {
         return (
-          <ul>
-            {resp.results.map(item => (
-              <Link
-                to={{
-                  pathname: "/movie",
-                  state: { movie: item}
-                }}
-              >
-                <li key={item.name}>
-                  {item.title} {item.vote_average}
-                </li>
-              </Link>
-            ))}
-          </ul>
+          <Container>
+            <Row className="justify-content-center">
+              <Col sm={12}>
+                <h1>Popular</h1>
+                <hr></hr>
+              </Col>
+
+              {resp.results.map(item => (
+                <Link
+                  to={{
+                    pathname: "/movie",
+                    state: { movie: item}
+                  }}
+                  style={styles.movieCard}
+                >
+                  <Col>
+                    <Card style={{ width: '15rem' }}>
+                      <Card.Img variant="top" src={`https://image.tmdb.org/t/p/w500${item.poster_path}`} />
+                      <Card.Body>
+                        <Card.Title>{item.title} {item.vote_average}</Card.Title>
+                      </Card.Body>
+                    </Card>
+                  </Col>
+                </Link>
+              ))}
+            </Row>
+          </Container>
+          // <ul>
+          //   {resp.results.map(item => (
+          //     <Link
+          //       to={{
+          //         pathname: "/movie",
+          //         state: { movie: item}
+          //       }}
+          //     >
+          //       <li key={item.name}>
+          //         {item.title} {item.vote_average}
+          //       </li>
+          //     </Link>
+          //   ))}
+          // </ul>
         );
       }
     }
