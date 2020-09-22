effafb0f39fd304664f10400b8712a2793fce690
Zac Holland <zacharyholland@gmail.com>
2020-09-22 16:33:02 -0700

    inputs use structs in transcoder

--- internal/app/http/transcoder.go
+++ internal/app/http/transcoder.go
@@ -9,7 +9,6 @@ import (
 	"strconv"
 	"syscall"
 
-	"github.com/diericx/iceetime/internal/app"
 	"github.com/gin-gonic/gin"
 )
 
@@ -17,17 +16,27 @@ func (h *HTTPHandler) addTranscoderGroup(rg *gin.RouterGroup) {
 	torrents := rg.Group("/transcoder")
 	{
 		torrents.GET("/from_url", func(c *gin.Context) {
-			url := c.Query("url")
-			timeArg := c.Query("time")
-			resolution := c.DefaultQuery("res", app.DefaultResolution)
-			maxBitrate := c.DefaultQuery("max_bitrate", app.DefaultMaxBitrate)
+			type Input struct {
+				URL        string `form:"url" binding:"required"`
+				Time       string `form:"time"`
+				Resolution string `form:"res" binding:"required"`
+				MaxBitrate string `form:"max_bitrate" binding:"required"`
+			}
+			var input Input
+
+			if err := c.ShouldBind(&input); err != nil {
+				c.JSON(http.StatusOK, gin.H{
+					"error": err.Error(),
+				})
+				return
+			}
 
 			// Format time arg
-			formattedTimeStr := formatTimeString(timeArg)
+			formattedTimeStr := formatTimeString(input.Time)
 
 			c.Writer.Header().Set("Transfer-Encoding", "chunked") // TODO: Is this necessary? not really sure what it does
 
-			cmdFF := h.Transcoder.NewTranscodeCommand(url, formattedTimeStr, resolution, maxBitrate, 0, 0)
+			cmdFF := h.Transcoder.NewTranscodeCommand(input.URL, formattedTimeStr, input.Resolution, input.MaxBitrate, 0, 0)
 			cmdFF.Stdout = c.Writer
 			cmdFF.Start()
 
@@ -53,13 +62,25 @@ func (h *HTTPHandler) addTranscoderGroup(rg *gin.RouterGroup) {
 				log.Println("Error encoding: ", err.Error())
 			}
 		})
+
 		torrents.GET("/from_url/metadata", func(c *gin.Context) {
-			url := c.Query("url")
-			out, err := exec.Command("ffprobe", "-v", "error", "-show_entries", "format=duration", "-of", "default=noprint_wrappers=1:nokey=1", url).Output()
+			type Input struct {
+				URL string `form:"url" binding:"required"`
+			}
+			var input Input
+
+			if err := c.ShouldBind(&input); err != nil {
+				c.JSON(http.StatusOK, gin.H{
+					"error": err.Error(),
+				})
+				return
+			}
+
+			out, err := exec.Command("ffprobe", "-v", "error", "-show_entries", "format=duration", "-of", "default=noprint_wrappers=1:nokey=1", input.URL).Output()
 			if err != nil {
 				c.JSON(http.StatusInternalServerError, gin.H{
 					"error":   true,
-					"message": fmt.Sprintf("Error fetching metadata", err.Error()),
+					"message": fmt.Sprintf("Error fetching metadata: %s", err.Error()),
 				})
 				return
 			}
@@ -71,7 +92,7 @@ func (h *HTTPHandler) addTranscoderGroup(rg *gin.RouterGroup) {
 				log.Println(err)
 				c.JSON(http.StatusInternalServerError, gin.H{
 					"error":   true,
-					"message": fmt.Sprintf("Error fetching metadata", err.Error()),
+					"message": fmt.Sprintf("Error fetching metadata: %s", err.Error()),
 				})
 				return
 			}
